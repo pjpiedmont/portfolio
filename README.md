a0db69620978ad0d3fcbd39e685d549df23d9a86
Parker Piedmont <pjpiedmont@proton.me>
2023-08-17 18:13:53 -0700

    Removed all the things

--- README.md
+++
@@ -1,5 +0,0 @@
-# Parker Piedmont's Portfolio Website
-
-This is a quick and dirty implementation of my portfolio website written in plain HTML with Bootstrap 5.3.0 alpha-1. This website shows off the music I have composed.
-
-Go to [parkerpiedmont.com](https://www.parkerpiedmont.com/) to see my portfolio.
--- js/margins.js
+++
@@ -1,9 +0,0 @@
-const main = document.querySelector('main');
-const navbar = document.querySelector('nav');
-const footer = document.querySelector('footer');
-
-const setMainMargins = () => {
-	main.setAttribute('style', `margin-top: ${navbar.offsetHeight}px; margin-bottom: ${footer.offsetHeight}px`);
-}
-
-setMainMargins();
